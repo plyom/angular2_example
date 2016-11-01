705d986137bd9fbaf7738f73320f5478dfe0923d
Fernando Pileggi <fernando@plyom.com.br>
2016-11-01 15:46:53 -0200

    organize configuration files inside a directory

--- config/systemjs.config.js
+++ config/systemjs.config.js
@@ -2,12 +2,12 @@
     System.config({
         paths: {
             // paths serve as alias
-            'npm:': 'node_modules/'
+            'npm:': '../node_modules/'
         },
         // map tells the System loader where to look for things
         map: {
             // our app is within the app folder
-            app: 'src',
+            app: '../src',
             // angular bundles
             '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
             '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
